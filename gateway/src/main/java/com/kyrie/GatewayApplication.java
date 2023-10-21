f12c5561b2332dceb02ff547a550ccc2aa35b998
jijinliang <625103471@qq.com>
2023-10-22 00:44:41 +0800

    网关服务 - 创建启动类

---
+++ gateway/src/main/java/com/kyrie/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.kyrie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: jijin
+ * @date: 2023/10/22 0:43 周日
+ * @project_name: skywolf-project
+ * @version: 1.0
+ * @description TODO
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
